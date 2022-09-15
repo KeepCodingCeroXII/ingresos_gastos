690a7f5ff3e21c39502c4b06b73240b24cc42c57
Món Maldonado <KeepCodingMon@MacBook-Air-de-Raquel-2.local>
2022-09-15 23:30:37 +0200

    templates con base y extends

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from registro_ig import app
 
 
 @app.route("/")
 def index():
-    return "Servidor rulando"
+    return render_template("index.html", pageTitle="Lista", movimientos=[])
+
+@app.route("/nuevo")
+def alta():
+    return render_template("new.html", pageTitle="Alta")
