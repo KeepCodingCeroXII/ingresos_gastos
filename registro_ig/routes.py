7210bf0aab569596946f3ea1edee30f415cd86e7
Món Maldonado <KeepCodingMon@MacBook-Air-de-Raquel-2.local>
2022-09-19 23:32:03 +0200

    Nuestro primer form para alta

--- registro_ig/routes.py
+++ registro_ig/routes.py
@@ -1,11 +1,33 @@
-from flask import render_template
+from flask import render_template, request, redirect
+import csv
 from registro_ig import app
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", pageTitle="Lista", movimientos=[])
+    fichero = open("data/movimientos.txt", "r")
+    csvReader = csv.reader(fichero, delimiter=',', quotechar='"')
+    movimientos = []
+    for movimiento in csvReader:
+        movimientos.append(movimiento)
 
-@app.route("/nuevo")
+    # movimientos = [movimiento for movimiento in csvReader] #list comprehension
+
+    fichero.close()
+    return render_template("index.html", pageTitle="Lista", movements=movimientos)
+
+@app.route("/nuevo", methods=["GET", "POST"])
 def alta():
-    return render_template("new.html", pageTitle="Alta")
+    if request.method == "GET":
+        return render_template("new.html", pageTitle="Alta")
+    else:
+        fichero = open("data/movimientos.txt", "a", newline="")
+        csvWriter = csv.writer(fichero, delimiter=",", quotechar='"')
+
+        #1ª validar que request.form.date <= hoy 
+        #si date > hoy devolver el formulario vacío
+
+        csvWriter.writerow([request.form['date'], request.form['concept'], request.form['quantity']])
+        fichero.close()
+
+        return redirect("/")
